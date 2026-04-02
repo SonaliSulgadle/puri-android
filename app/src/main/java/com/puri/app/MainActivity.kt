6aba999dfe8ee10e42af79baa83521668daf17e3
Sonali Sulgadle <sonali.sulgadle@gmail.com>
2026-04-02 21:26:01 +0900

    Puri theme with font and color palette added

--- app/src/main/java/com/puri/app/MainActivity.kt
+++ app/src/main/java/com/puri/app/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.puri.app.ui.theme.PuriTheme
+import com.puri.app.core.ui.theme.PuriTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
